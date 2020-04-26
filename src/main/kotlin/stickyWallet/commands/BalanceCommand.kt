5188bc59a85e9e0263660919f698d5e4d01abcf8
Vlad Frangu <kingdgrizzle@gmail.com>
2020-04-26 04:09:27 +0300

    Revert BalanceCommand.kt

--- src/main/kotlin/stickyWallet/commands/BalanceCommand.kt
+++ src/main/kotlin/stickyWallet/commands/BalanceCommand.kt
@@ -21,7 +21,6 @@ class BalanceCommand : TabExecutor {
 
             val account: Account? = if (args.isEmpty() && sender is Player) {
                 plugin.accountManager.getAccount(sender)
-
             } else if (sender.hasPermission(Permissions.COMMAND_BALANCE_OTHER) && args.size == 1) {
                 plugin.accountManager.getAccount(args[0])
             } else {
